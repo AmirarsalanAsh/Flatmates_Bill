1447350e3885cd97781f306d1cc77ee82c528d10
Amir Arsalan Asharion <Amirarsalan.Asharion@beltios.com>
2023-03-20 17:14:43 +0100

    pays method completed

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ class Flatmate:
     a share of the bill.
     """
 
-    def __int__(self, name, days_in_house):
+    def __init__(self, name, days_in_house):
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        return bill / 2
+    def pays(self, bill,flatmate2):
+        weight = self.days_in_house/ (self.days_in_house + flatmate2.days_in_house)
+
+        to_pay = weight * bill.amount
+
+        return to_pay
 
 
 class PdfReport:
@@ -38,6 +42,9 @@ class PdfReport:
 
 
 the_bill = Bill(amount=120, period='March 2021')
+john = Flatmate(name="John", days_in_house=20)
+marry = Flatmate (name="Marry", days_in_house= 25)
+print(" John pays: ", john.pays(bill=the_bill, flatmate2=marry))
+print(" Marry pays: ", marry.pays(bill=the_bill, flatmate2=john))
 
-john = Flatmate(name="John")
 
